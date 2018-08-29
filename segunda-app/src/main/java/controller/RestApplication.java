86a36572ee23e28d4dcd2df858d5ff91d65f1622
Thiago da Costa Pinto Frazao de Sousa <e514189@corp.caixa.gov.br>
2018-08-29 15:54:37 -0300

    Removendo Primeira-API

--- segunda-app/src/main/java/controller/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application {
-
-}
