3fb4e03caf23abac81d47481bb7b4df1c60a1dff
Thiago Frazao de Sousa <e514189@mail.caixa>
2019-01-29 16:38:22 -0200

    ultima aula

--- alurajavaee/src/main/java/javaee/loja/conf/JaxRsConfig.java
+++
@@ -1,9 +0,0 @@
-package javaee.loja.conf;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/services")
-public class JaxRsConfig extends Application {
-
-}
--- alurajavaee/src/main/java/javaee/loja/models/Compra.java
+++ alurajavaee/src/main/java/javaee/loja/models/Compra.java
@@ -24,6 +24,7 @@ public class Compra {
 	private String itensCompra;
 	
 	private String uuId;
+	
 
 	public Compra(){
 		
